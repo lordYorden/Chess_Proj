2d5de1ce529223cce803be7a926ef4ccbf83143a
yarden perets <yordenperets@gmail.com>
2020-12-21 19:33:10 +0200

    changed knight constructor

--- chessProject/Project/Knight.cpp
+++ chessProject/Project/Knight.cpp
@@ -1,11 +1,19 @@
-#include "Knight.h"
-
 #pragma once
+#include "Knight.h"
+#define BLACK 'k'
+#define WHITE 'K'
 
 Knight::Knight(std::string& place, bool isWhite):
 	Piece(place, isWhite)
 {
-
+	if (this->_isWhite)
+	{
+		setValue(WHITE);
+	}
+	else
+	{
+		setValue(BLACK);
+	}
 }
 
 Knight::~Knight()
