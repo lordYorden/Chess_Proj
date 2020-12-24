53a7e7a60fcd73db8aea8fa39f8265698e01995d
Guy Glick <guyglick27@gmai.com>
2020-12-24 11:51:17 +0200

    Revert "meow(fixed Rook isLegal)"
    This reverts commit 6987d183fb8e5b80a4116efe99249b2c5772b65a.

--- chessProject/Project/Rook.cpp
+++ chessProject/Project/Rook.cpp
@@ -38,9 +38,9 @@ bool Rook::inWay(Piece* board[8][8], std::string& dst)
 	{
 		if (dst[0] - 'a' > this->_position[1])
 		{
-			for (int i = this->_position[1]+1; i <= dst[0] - 'a'; i++) //checking forward
+			for (int i = this->_position[1]; i <= dst[0] - 'a'; i++) //checking forward
 			{
-				
+				std::cout << "horizontalf" << std::endl;
 				if (board[this->_position[0]][i]->isPieceWhite() == this->isPieceWhite() && board[this->_position[0]][i]->getValue() != '#')
 				{
 					answer = true;
@@ -49,9 +49,9 @@ bool Rook::inWay(Piece* board[8][8], std::string& dst)
 		}
 		else
 		{
-			for (int i =dst[0]-'a'+1;i >= this->_position[1]; i--) //checking backwards
+			for (int i =dst[0]-'a';i >= this->_position[1]; i--) //checking backwards
 			{
-				
+				std::cout << "horizontalb" << std::endl;
 				if (board[this->_position[0]][i]->isPieceWhite() == this->isPieceWhite() && board[this->_position[0]][i]->getValue() != '#')
 				{
 					answer = true;
@@ -64,10 +64,10 @@ bool Rook::inWay(Piece* board[8][8], std::string& dst)
 	{
 		if (dst[1] - '1' > this->_position[0]) //checking forwards
 		{
-			for (int i = this->_position[0]+1; i <= dst[1] - '1' && !answer; i++)
+			for (int i = this->_position[0]; i <= dst[1] - '1' && !answer; i++)
 			{
-				
-				if ((board[i][this->_position[1]]->getValue() != '#'))
+				std::cout << "verticalf" << std::endl;
+				if (board[i][this->_position[1]]->isPieceWhite() == this->isPieceWhite() && (board[i][this->_position[1]]->getValue() != '#'))
 				{
 					answer = true;
 				}
@@ -75,9 +75,9 @@ bool Rook::inWay(Piece* board[8][8], std::string& dst)
 		}
 		else
 		{
-			for (int i = dst[1]-'1'+1 ; i >= this->_position[0] && !answer; i--) //checking checking backwards
+			for (int i = dst[1]-'1' ; i >= this->_position[0] && !answer; i--) //checking checking backwards
 			{
-				
+				std::cout << "verticalb" << std::endl;
 				if (board[i][this->_position[1]]->isPieceWhite() == this->isPieceWhite() && (board[i][this->_position[1]]->getValue() != '#'))
 				{
 					answer = true;
