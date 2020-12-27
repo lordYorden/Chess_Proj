eabc4f6b59989710f3d4af637ec7892f47c87fe2
Guy Glick <guyglick27@gmai.com>
2020-12-27 20:09:13 +0200

    Added documentation and defines

--- chessProject/Project/Rook.cpp
+++ chessProject/Project/Rook.cpp
@@ -1,8 +1,16 @@
 #include "Rook.h"
 #define BLACK 'r'
 #define WHITE 'R'
-Rook::Rook(int x,int y, bool isWhite) :
-	Piece(x,y, isWhite)
+#define OUTRANGE 5
+#define SAME 7
+#define ISNTMINE 2
+#define ISNTFREE 3
+#define INVALID 6
+/*
+* rook c'tor,
+*/
+Rook::Rook(int x,int y, bool isWhite) : 
+	Piece(x,y, isWhite) //inits x and y position and value matching the color
 {
 	if (this->_isWhite)
 	{
@@ -13,6 +21,11 @@ Rook::Rook(int x,int y, bool isWhite) :
 		setValue(BLACK);
 	}
 }
+/*
+* function that checks if a rook's move is legal
+* input: board with pieces, dst location
+* output: opCode
+*/
 int Rook::isLegal(Piece* board[8][8], std::string& dst)
 {
 	int answer = 0;
@@ -20,21 +33,25 @@ int Rook::isLegal(Piece* board[8][8], std::string& dst)
 	int x = dst[0] - 'a';
 	std::cout << board[y][x]->getValue() << std::endl;;
 	std::cout << board[this->_position[0]][this->_position[1]]->getValue() << std::endl;
-	if ((dst[0] > 'h' || dst[0] < 'a') || (dst[1] < '1' || dst[1] > '8'))answer = 5;
-	else if (board[this->_position[0]][this->_position[1]]->getValue() == '#') answer = 2;
-	else if (x == this->getPositionX() && y == this->getPositionY()) answer = 7;
-	else if (board[y][x]->getValue() != '#' && this->_isWhite == board[y][x]->isPieceWhite())answer = 3;
-	else if ((this->_position[0] != y && this->_position[1] != x) || this->inWay(board, dst))answer = 6;
+	if ((dst[0] > 'h' || dst[0] < 'a') || (dst[1] < '1' || dst[1] > '8'))answer = OUTRANGE; //out of range
+	else if (board[this->_position[0]][this->_position[1]]->getValue() == '#') answer = ISNTMINE; //isnt my piece
+	else if (x == this->getPositionX() && y == this->getPositionY()) answer = SAME; //dst same as src
+	else if (board[y][x]->getValue() != '#' && this->_isWhite == board[y][x]->isPieceWhite())answer = ISNTFREE; //cell isn't free, containing piece of same color
+	else if ((this->_position[0] != y && this->_position[1] != x) || this->inWay(board, dst))answer = INVALID;//invalid movement
 	else answer = 0;
 	return answer;
 }
-
+/*
+* function that checks if a piece is found in a way of a rook that wants to move to that location
+* input: board of pieces, dst location
+* output: true/false
+*/
 bool Rook::inWay(Piece* board[8][8], std::string& dst)
 {
 	bool answer = false;
-	if (this->_position[0] == dst[1] - '1') //checking horzontally
+	if (this->_position[0] == dst[1] - '1') //checking horizontally
 	{
-		if (dst[0] - 'a' > this->_position[1])
+		if (dst[0] - 'a' > this->_position[1]) //if we need to chek left
 		{
 			for (int i = this->_position[1]+1; i < dst[0] - 'a' && !answer; i++) //checking forward
 			{
@@ -45,7 +62,7 @@ bool Rook::inWay(Piece* board[8][8], std::string& dst)
 				}
 			}
 		}
-		else
+		else //if we need to check right
 		{
 			for (int i = this->_position[1]-1; i > dst[0] - 'a' && !answer; i--) //checking backwards
 			{
@@ -60,7 +77,7 @@ bool Rook::inWay(Piece* board[8][8], std::string& dst)
 	}
 	else
 	{
-		if (dst[1] - '1' > this->_position[0]) //checking forwards
+		if (dst[1] - '1' > this->_position[0]) //checking vetically up
 		{
 			for (int i = this->_position[0]+1; i < dst[1] - '1' && !answer; i++)
 			{
@@ -73,7 +90,7 @@ bool Rook::inWay(Piece* board[8][8], std::string& dst)
 		}
 		else if(dst[1] - '1' < this->_position[0])
 		{
-			for (int i = this->_position[0]-1  ; i > dst[1] - '1' && !answer; i--) //checking checking backwards
+			for (int i = this->_position[0]-1  ; i > dst[1] - '1' && !answer; i--) //checking verticall down
 			{
 				std::cout << "vertical" << std::endl;
 				if ((board[i][this->_position[1]]->getValue() != '#') )
--- chessProject/Project/Rook.h
+++ chessProject/Project/Rook.h
@@ -4,8 +4,8 @@
 class Rook : public Piece
 {
 public:
-	Rook(int x,int y, bool isWhite);
-	virtual int isLegal(Piece* board[8][8], std::string& dst);
+	Rook(int x,int y, bool isWhite); //c'tor
+	virtual int isLegal(Piece* board[8][8], std::string& dst); //if a rook's move is legal
 private:
-	bool inWay(Piece* board[8][8], std::string& dst);
+	bool inWay(Piece* board[8][8], std::string& dst); //if a piece is found in a way of a moving rook
 };
